aa7d761b237a5ea7f15546b13901e51fe808f906
mateuszskibicki <skibickiengland@gmail.com>
2021-11-04 12:44:07 +0000

    Some more options how to log :)

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import inquirer from 'inquirer';
 import { getFizzBuzzObject } from './get-fizz-buzz.function';
 
+enum Syntax {
+  Object = 'OBJECT',
+  Array = 'ARRAY',
+  ForLoop = 'FOR_LOOP',
+}
+
 const logFizzBuzz = async (): Promise<void> => {
-  const { length } = await inquirer.prompt([
+  const { length, syntax } = await inquirer.prompt([
     {
       type: 'number',
       name: 'length',
@@ -10,9 +16,21 @@ const logFizzBuzz = async (): Promise<void> => {
       default: 100,
       validate: (e): boolean => !!e && Number.isInteger(Number(e)),
     },
+    {
+      type: 'list',
+      name: 'syntax',
+      message: 'Syntax:',
+      choices: Object.values(Syntax),
+    },
   ]);
 
-  console.log(JSON.stringify(getFizzBuzzObject(length), null, 4));
+  if (syntax === Syntax.Object)
+    console.log(JSON.stringify(getFizzBuzzObject(length), null, 4));
+  if (syntax === Syntax.Array)
+    console.log(Object.entries(getFizzBuzzObject(length)));
+  if (syntax === Syntax.ForLoop)
+    for (const value of Object.values(getFizzBuzzObject(length)))
+      console.log(value);
 };
 
 logFizzBuzz();
